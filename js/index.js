d6843823694aa6ea16b54735a0a5346368ec0146
ikeman32 <ikeman32@yaho.com>
2019-10-01 12:32:09 -0700

    adding events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+const busClick = document.querySelector("img");
+
+
+window.addEventListener("load", () => {
+    busClick.style.transform = "scale( 1.2)";
+    busClick.style.transition = "all 2s";
+});
+
+busClick.addEventListener("mouseover", () => {
+    busClick.style.transform = "scale( 1.0)";
+    busClick.style.transition = "all 2s";
+});
+
+busClick.addEventListener("click", () => {
+    busClick.style.transform = "scale( 2.5)";
+    busClick.style.transition = "all 3s";
+});
+
+const body = document.querySelector('body');
+
+body.addEventListener("wheel", () => {
+    body.style.backgroundColor = "purple";
+});
